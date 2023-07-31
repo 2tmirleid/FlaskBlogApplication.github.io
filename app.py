4c5e2f5a3c5e23c3b55ee1fc3c3c3aa82742c848
alex <alex1levinov1@gmail.com>
2023-07-31 18:07:52 +0300

    #Added smth on 19 str

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 from flask import *
 
-
 app = Flask(__name__)
 
 menu = [{"name": "Установка", "url": "#"},
         {"name": "Первое приложение", "url": "#"},
         {"name": "Обратная связь", "url": "/contact"}]
 
+
 @app.route("/")
 def index():
     print(url_for("index"))
     return render_template("index.html", title="Главная", menu=menu)
 
-@app.route("/contact", methods=["POST"])
+#Added 19 string
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
+    if request.method == "POST":
+        print(request.form['username'], request.form['email'], request.form['message'])
+
     return render_template("contact.html", title="Обратная связь", menu=menu)
+
 
 @app.route("/test/<username>")
 def test(username):
     return f'Пользователь: {username}'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
