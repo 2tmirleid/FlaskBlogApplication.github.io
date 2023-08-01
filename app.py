75ddb0a49f97240b80c7123df906284d71886622
alex <alex1levinov1@gmail.com>
2023-08-01 15:14:07 +0300

    #finish

--- app.py
+++ app.py
@@ -1,67 +1,63 @@
 from flask import *
 import sqlite3
 import os
-
+import db_settings
 from db_settings import *
+import FDataBase
+from FDataBase import *
 
 app = Flask(__name__)
-
-menu = [{"name": "Установка", "url": "#"},
-        {"name": "Первое приложение", "url": "#"},
-        {"name": "Обратная связь", "url": "/contact"},
-        {"name": "Авторизация", "url": "/login"}]
+app.secret_key = db_settings.SECRET_KEY
 
 
 @app.route("/")
 def index():
     db = get_db()
-    return render_template("index.html", title="Главная", menu=menu)
+    dbase = FDataBase(db)
+    return render_template("index.html", title="Главная", menu=dbase.getMenu(), posts=dbase.getPostsAnnounce())
 
 
-@app.route("/contact", methods=["POST", "GET"])
-def contact():
-    if request.method == "POST":
-        if len(request.form['username']) > 2:
-            flash('Сообщение отправлено!', category='success')
+@app.route("/add_post", methods=['POST', 'GET'])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка при добавлении статьи', category='error')
+            else:
+                flash('Статья успешно добавлена', category='success')
         else:
-            flash('Введите корректное имя', category='error')
+            flash('Ошибка при добавлении статьи', category='error')
 
-        print(request.form['username'], request.form['email'], request.form['message'])
-    return render_template("contact.html", title="Обратная связь", menu=menu)
-
-
-@app.route('/profile/<username>')
-def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(401)
-
-    return render_template('profile.html', name=username)
+    return render_template('add_post.html', menu=dbase.getMenu(), title='Добавление статьи')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == '2tmirleid' and request.form['pass'] == '123':
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
 
-    return render_template('login.html', title="Авторизация", menu=menu)
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html', title="Страница не найдена", menu=menu), 404
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('404.html', title="Страница не найдена", menu=dbase.getMenu()), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error):
-    return render_template('401.html', title="Не авторизован", menu=menu), 401
-
-
-@app.route("/test/<username>")
-def test(username):
-    return f'Пользователь: {username}'
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('401.html', title="Не авторизован", menu=dbase.getMenu()), 401
 
 
 if __name__ == "__main__":
